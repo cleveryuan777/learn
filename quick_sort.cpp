15b0779e0abf8ae81fd6ee0b13df401c39d6dda2
yuan <cleveryuan777@gmail.com>
2023-05-10 20:56:56 +0800

    complete find_num_k

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,10 +1,9 @@
 #include <algorithm>
 #include <iostream>
-#include <math.h>
-#include <stdio.h>
+
 using namespace std;
 
-int part(int* arr, int low, int high)
+int part(int* arr, int low, int high, int )
 {
   int i = low, j = high, pivot = arr[low]; //基准元素
 
@@ -34,7 +33,20 @@ int part(int* arr, int low, int high)
   return i;
 
 }
-
+int part(int* arr, int low, int high)
+{
+  int pivot = arr[high];
+  int i = low;
+  for (int j = low; j < high; ++j)
+  {
+    if (arr[j] < pivot)
+    {
+      swap(arr[i++], arr[j]);
+    }
+  }
+  swap(arr[i], arr[high]);
+  return i;
+}
 
 void quick_sort(int* arr, int low, int high)
 {
@@ -48,21 +60,61 @@ void quick_sort(int* arr, int low, int high)
 }
 
 
+int find_num_k(int* arr, int low, int high, int k)
+{
+  int mid;
+  if(low < high)
+  {
+    mid = part(arr, low, high);
+    if (mid + 1 < k)
+    {
+      find_num_k(arr, mid + 1, high, k);
+    }
+    else if (mid + 1 > k)
+    {
+      find_num_k(arr, low, mid - 1, k);
+    }
+    else
+    {
+      return arr[mid];
+    }
+  }
+}
+
+
+//int main() {
+//  int a[10001];
+//  int N;
+//  cout << "请输入要排序的数据的个数： " << endl;
+//  cin >> N;
+//  cout << "请输入要排序的数据： " << endl;
+//  for (int i = 0; i < N; i++) {
+//    cin >> a[i];
+//  }
+//  cout << endl;
+//  quick_sort(a, 0, N - 1);
+//  cout << "排序后的序列为： " << endl;
+//  for (int i = 0; i < N; i++) {
+//    cout << a[i] << " ";
+//  }
+//  cout << endl;
+//  return 0;
+//}
+
 int main() {
   int a[10001];
   int N;
-  cout << "请输入要排序的数据的个数： " << endl;
+  cout << "请输入要数据个数： " << endl;
   cin >> N;
-  cout << "请输入要排序的数据： " << endl;
+  cout << "请输入数据： " << endl;
   for (int i = 0; i < N; i++) {
     cin >> a[i];
   }
+  cout << "请输入需要查找第几大的数： " << endl;
+  int K;
+  cin >> K;
+  cout << "结果是" << find_num_k(a, 0, N - 1, K);
   cout << endl;
-  quick_sort(a, 0, N - 1);
-  cout << "排序后的序列为： " << endl;
-  for (int i = 0; i < N; i++) {
-    cout << a[i] << " ";
-  }
-  cout << endl;
+
   return 0;
 }
